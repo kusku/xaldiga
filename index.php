515e8729323218c6a18517c00ba11901e07fd1f1
Marc Cuscullola <mcuscullola@gmail.com>
2019-03-29 18:49:44 +0100

    Using php instead of react

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('src/header.html'); ?>
+  
+  <body>
+	<!-- Header -->
+	<?php include('src/components/navbar.html'); ?>
+
+	<!-- Content -->
+	<!-- https://startbootstrap.com/snippets/full-slider/ -->
+	<div class="header-content">
+		<div id="carouselExampleIndicators" class="carousel" data-ride="carousel">
+			<div class="carousel-item active" >
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="container">
+			<div id="relaod_test"></div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<?php include('src/components/footer.html'); ?>
+
+	<?php include('src/scriptsdef.html'); ?>
+
+  </body>
+</html>
