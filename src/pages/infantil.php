ffc56e6821e202f2d80c136b7c1bb142a2ca5ab1
Marc Cuscullola <mcuscullola@gmail.com>
2019-04-24 23:35:39 +0200

    Adding content to infantil page

--- src/pages/infantil.php
+++ src/pages/infantil.php
@@ -1,7 +1,20 @@
 <div class="generic-content">
-	<img src="/images/infantil.jpg" class="center-block main-img">
-	<div class="container-fluid">
+	<div class="cropped-portrait">
+		<img src="/images/infantil/main-image.jpg" class="center-block">
+		<div class="container portrait-caption">
+			INFANTIL
+		</div>
+	</div>
+	<div class="container-fluid content-container">
 		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<p>Arran de la celebració de la 1a Moscada infantil de la Festa Major de Manresa l’any 2004 es va crear la Secció Infantil de Xàldiga, amb una doble finalitat: d’una banda iniciar a un grup de nois i noies en els diferents aspectes vinculats a les festes tradicionals de foc –percussions, pirotècnia, diables, imatgeria, etc.- tot afavorint la seva participació i continuïtat en l’entitat i, per altra banda, acostar les festes de foc als infants i adolescents de Manresa. Durant aquests anys de funcionament la Secció Infantil ha anat consolidant una estructura i un funcionament propi, i ha anat definint i creant els elements propis i identitaris com a colla de foc infantil. En paral·lel, també ha consolidat la seva participació en les dues principals festes de la ciutat: La Fira de l’Aixada i la Festa Major de Manresa. En aquest segon cas es participa en el recorregut del cercavila i s’organitza una activitat pròpia: la Moscada Infantil. A més a més, la Secció participa en altres esdeveniments de la ciutat i en altres festes i manifestacions de foc i bestiari que s’organitzen arreu de Catalunya.</p>
+				</div>
+				<div class="col-lg-6 content-image">
+					<img src="/images/infantil/moscada-infantil.jpg" alt="Moscada Infantil">
+				</div>
+			</div>
 			<div class="row">
 				<div class="col-lg-3">
 					<h1>Imatge</h1>
