57fa09944128fc62d8503e5c58ec3e55c6a02c72
Marc Cuscullola <mcuscullola@gmail.com>
2019-05-03 19:52:23 +0200

    Added fonts

--- src/pages/infantil.php
+++ src/pages/infantil.php
@@ -8,7 +8,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6">
-					<p>Arran de la celebració de la 1a Moscada infantil de la Festa Major de Manresa l’any 2004 es va crear la Secció Infantil de Xàldiga, amb una doble finalitat: d’una banda iniciar a un grup de nois i noies en els diferents aspectes vinculats a les festes tradicionals de foc –percussions, pirotècnia, diables, imatgeria, etc.- tot afavorint la seva participació i continuïtat en l’entitat i, per altra banda, acostar les festes de foc als infants i adolescents de Manresa. Durant aquests anys de funcionament la Secció Infantil ha anat consolidant una estructura i un funcionament propi, i ha anat definint i creant els elements propis i identitaris com a colla de foc infantil. En paral·lel, també ha consolidat la seva participació en les dues principals festes de la ciutat: La Fira de l’Aixada i la Festa Major de Manresa. En aquest segon cas es participa en el recorregut del cercavila i s’organitza una activitat pròpia: la Moscada Infantil. A més a més, la Secció participa en altres esdeveniments de la ciutat i en altres festes i manifestacions de foc i bestiari que s’organitzen arreu de Catalunya.</p>
+					<p class="p-desc">Arran de la celebració de la 1a Moscada infantil de la Festa Major de Manresa l’any 2004 es va crear la Secció Infantil de Xàldiga, amb una doble finalitat: d’una banda iniciar a un grup de nois i noies en els diferents aspectes vinculats a les festes tradicionals de foc –percussions, pirotècnia, diables, imatgeria, etc.- tot afavorint la seva participació i continuïtat en l’entitat i, per altra banda, acostar les festes de foc als infants i adolescents de Manresa. Durant aquests anys de funcionament la Secció Infantil ha anat consolidant una estructura i un funcionament propi, i ha anat definint i creant els elements propis i identitaris com a colla de foc infantil. En paral·lel, també ha consolidat la seva participació en les dues principals festes de la ciutat: La Fira de l’Aixada i la Festa Major de Manresa. En aquest segon cas es participa en el recorregut del cercavila i s’organitza una activitat pròpia: la Moscada Infantil. A més a més, la Secció participa en altres esdeveniments de la ciutat i en altres festes i manifestacions de foc i bestiari que s’organitzen arreu de Catalunya.</p>
 				</div>
 				<div class="col-lg-6 content-image">
 					<img src="/images/infantil/moscada-infantil.jpg" alt="Moscada Infantil">
@@ -28,7 +28,7 @@
 					<img src="/images/infantil/diablons.jpg" alt="Diablons">
 				</div>
 				<div class="col-lg-6">
-					<p>Els Diablons són els encarregats de fer l’espectacle de foc. Per fer-ho, utilitzen una forca amb la que tiren les carretilles o els sortidors, depenent de l’actuació.<br><br>
+					<p class="p-desc">Els Diablons són els encarregats de fer l’espectacle de foc. Per fer-ho, utilitzen una forca amb la que tiren les carretilles o els sortidors, depenent de l’actuació.<br><br>
 					Els Diablons, com els diables grans, tenen també un ball propi, estrenat  a la moscada de l’any 2008. La música del ball és de Lluís Toran, amb alguna variació realitzada durant els últims anys, i amb la coreografia de Maribel Jódar.<br><br>
 					L’edat mínima per participar amb el grup de Diablons és de 9 anys. 
 					</p>
@@ -44,7 +44,7 @@
 					<img src="/images/infantil/tabalons.jpg" alt="Tabalons">
 				</div>
 				<div class="col-lg-6">
-					<p>Els Tabalons són el grup de percussió de la secció infantil. Acostumen a acompanyar als diablons a totes les actuacions, tot i que també en fan ells pel seu compte.<br><br>
+					<p class="p-desc">Els Tabalons són el grup de percussió de la secció infantil. Acostumen a acompanyar als diablons a totes les actuacions, tot i que també en fan ells pel seu compte.<br><br>
 					El grup consta de diversos instruments: el “repe” (repenique), les caixes, els tabals, els bombos, la closca, el “xiqui”, els xiulets i les esquelles. Els diferents ritmes del grup es poden veure a les cercaviles que es fan, o bé als espectacles que es duen a terme, ja sigui estàtics o de recorregut.<br><br>
 					L’edat mínima per participar amb el grup de Tabalons és de 6 anys. 
 					</p>
@@ -60,7 +60,7 @@
 					<img src="/images/infantil/tremenda.jpg" alt="Tremenda">
 				</div>
 				<div class="col-lg-6">
-					<p>La Tremenda és el drac de la secció infantil. És una estructura que simula la silueta d’un drac de dos caps. Els colors de la Tremenda provenen de la Víbria i el Drac de Manresa, mare i pare de la Tremenda. La Tremenda, durant l’any recull els xumets dels infants de la ciutat i els porta al coll durant les actuacions.<br><br>
+					<p class="p-desc">La Tremenda és el drac de la secció infantil. És una estructura que simula la silueta d’un drac de dos caps. Els colors de la Tremenda provenen de la Víbria i el Drac de Manresa, mare i pare de la Tremenda. La Tremenda, durant l’any recull els xumets dels infants de la ciutat i els porta al coll durant les actuacions.<br><br>
 					El Ball de la Tremenda és costum que el faci un sol ballador, tot i que en alguns moments puntuals l’han arribat a ballar dues persones.<br><br>
 					Té dues pinces a la boca per on s’hi posa la pirotècnia durant el ball i els diferents correfocs que es fan durant l’any. Normalment porta 4 fuets encesos.<br><br>
 					Els portadores i les portadores del drac són infants de la secció. 
@@ -77,7 +77,7 @@
 					<img src="/images/infantil/traqueta.jpg" alt="Traqueta">
 				</div>
 				<div class="col-lg-6">
-					<p>WIP
+					<p class="p-desc">WIP
 					</p>
 				</div>
 			</div>
@@ -91,7 +91,7 @@
 					<img src="/images/infantil/bou.jpg" alt="Bou">
 				</div>
 				<div class="col-lg-6">
-					<p>WIP
+					<p class="p-desc">WIP
 					</p>
 				</div>
 			</div>
@@ -105,7 +105,7 @@
 					<img src="/images/infantil/ceptrot.jpg" alt="Ceptrot">
 				</div>
 				<div class="col-lg-6">
-					<p>WIP
+					<p class="p-desc">WIP
 					</p>
 				</div>
 			</div>
