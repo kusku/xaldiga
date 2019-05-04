a5f3446e34c540530dee7cf1fee094fdc5ad169d
Marc Cuscullola <mcuscullola@gmail.com>
2019-05-04 21:44:45 +0200

    Added favicon

--- src/layout.php
+++ src/layout.php
@@ -18,6 +18,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
 
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
         <meta name="description" content="Entitat manresana organitzadora d'actes festius, entre ells El Correfoc de Manresa.">
     </head>
   
