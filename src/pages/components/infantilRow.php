5d061a8ad6fc649e1e05eea378e3c9f96cfa6bf4
Marc Cuscullola <mcuscullola@gmail.com>
2019-05-07 19:17:57 +0200

    Using template for infantil rows

---
+++ src/pages/components/infantilRow.php
@@ -0,0 +1,13 @@
+<div class="row row-title-space">
+    <div class="col">
+        <h1><?php echo $sectionTitle ?></h1>
+    </div>
+</div>
+<div class="row row-margin">
+    <div class="col-lg-6 content-image">
+        <img src="<?php echo $sectionUrl ?>" alt="<?php echo $sectionTitle ?>">
+    </div>
+    <div class="col-lg-6">
+        <p class="p-desc"><?php echo $sectionDescription ?></p>
+    </div>
+</div>
--- src/pages/infantil.php
+++ src/pages/infantil.php
@@ -20,97 +20,45 @@
 	</div>
 	<div class="container-fluid">
 		<div class="container">
-			<div class="row">
-				<div class="col">
-					<h1>Diablons</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/diablons.jpg" alt="Diablons">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">Els Diablons són els encarregats de fer l’espectacle de foc. Per fer-ho, utilitzen una forca amb la que tiren les carretilles o els sortidors, depenent de l’actuació.<br><br>
-					Els Diablons, com els diables grans, tenen també un ball propi, estrenat  a la moscada de l’any 2008. La música del ball és de Lluís Toran, amb alguna variació realitzada durant els últims anys, i amb la coreografia de Maribel Jódar.<br><br>
-					L’edat mínima per participar amb el grup de Diablons és de 9 anys. 
-					</p>
-				</div>
-			</div>
-			<div class="row row-title-space">
-				<div class="col">
-					<h1>Tabalons</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/tabalons.jpg" alt="Tabalons">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">Els Tabalons són el grup de percussió de la secció infantil. Acostumen a acompanyar als diablons a totes les actuacions, tot i que també en fan ells pel seu compte.<br><br>
-					El grup consta de diversos instruments: el “repe” (repenique), les caixes, els tabals, els bombos, la closca, el “xiqui”, els xiulets i les esquelles. Els diferents ritmes del grup es poden veure a les cercaviles que es fan, o bé als espectacles que es duen a terme, ja sigui estàtics o de recorregut.<br><br>
-					L’edat mínima per participar amb el grup de Tabalons és de 6 anys. 
-					</p>
-				</div>
-			</div>
-			<div class="row row-title-space">
-				<div class="col">
-					<h1>Tremenda</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/tremenda.jpg" alt="Tremenda">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">La Tremenda és el drac de la secció infantil. És una estructura que simula la silueta d’un drac de dos caps. Els colors de la Tremenda provenen de la Víbria i el Drac de Manresa, mare i pare de la Tremenda. La Tremenda, durant l’any recull els xumets dels infants de la ciutat i els porta al coll durant les actuacions.<br><br>
-					El Ball de la Tremenda és costum que el faci un sol ballador, tot i que en alguns moments puntuals l’han arribat a ballar dues persones.<br><br>
-					Té dues pinces a la boca per on s’hi posa la pirotècnia durant el ball i els diferents correfocs que es fan durant l’any. Normalment porta 4 fuets encesos.<br><br>
-					Els portadores i les portadores del drac són infants de la secció. 
-					</p>
-				</div>
-			</div>
-			<div class="row row-title-space">
-				<div class="col">
-					<h1>Traqueta</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/traqueta.jpg" alt="Traqueta">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">WIP
-					</p>
-				</div>
-			</div>
-			<div class="row row-title-space">
-				<div class="col">
-					<h1>Bou</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/bou.jpg" alt="Bou">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">WIP
-					</p>
-				</div>
-			</div>
-			<div class="row row-title-space">
-				<div class="col">
-					<h1>Ceptrot</h1>
-				</div>
-			</div>
-			<div class="row row-margin">
-				<div class="col-lg-6 content-image">
-					<img src="/images/infantil/ceptrot.jpg" alt="Ceptrot">
-				</div>
-				<div class="col-lg-6">
-					<p class="p-desc">WIP
-					</p>
-				</div>
-			</div>
+			<?php 
+				$sectionTitle="Diablons";
+				$sectionDescription="Els Diablons són els encarregats de fer l’espectacle de foc. Per fer-ho, utilitzen una forca amb la que tiren les carretilles o els sortidors, depenent de l’actuació.<br><br>
+				Els Diablons, com els diables grans, tenen també un ball propi, estrenat  a la moscada de l’any 2008. La música del ball és de Lluís Toran, amb alguna variació realitzada durant els últims anys, i amb la coreografia de Maribel Jódar.<br><br>
+				L’edat mínima per participar amb el grup de Diablons és de 9 anys.";
+				$sectionUrl="/images/infantil/diablons.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+
+
+				$sectionTitle="Tabalons";
+				$sectionDescription="Els Tabalons són el grup de percussió de la secció infantil. Acostumen a acompanyar als diablons a totes les actuacions, tot i que també en fan ells pel seu compte.<br><br>
+				El grup consta de diversos instruments: el “repe” (repenique), les caixes, els tabals, els bombos, la closca, el “xiqui”, els xiulets i les esquelles. Els diferents ritmes del grup es poden veure a les cercaviles que es fan, o bé als espectacles que es duen a terme, ja sigui estàtics o de recorregut.<br><br>
+				L’edat mínima per participar amb el grup de Tabalons és de 6 anys.";
+				$sectionUrl="/images/infantil/tabalons.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+
+				$sectionTitle="Tremenda";
+				$sectionDescription="La Tremenda és el drac de la secció infantil. És una estructura que simula la silueta d’un drac de dos caps. Els colors de la Tremenda provenen de la Víbria i el Drac de Manresa, mare i pare de la Tremenda. La Tremenda, durant l’any recull els xumets dels infants de la ciutat i els porta al coll durant les actuacions.<br><br>
+				El Ball de la Tremenda és costum que el faci un sol ballador, tot i que en alguns moments puntuals l’han arribat a ballar dues persones.<br><br>
+				Té dues pinces a la boca per on s’hi posa la pirotècnia durant el ball i els diferents correfocs que es fan durant l’any. Normalment porta 4 fuets encesos.<br><br>
+				Els portadores i les portadores del drac són infants de la secció.";
+				$sectionUrl="/images/infantil/tremenda.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+
+				$sectionTitle="Traqueta";
+				$sectionDescription="WIP";
+				$sectionUrl="/images/infantil/traqueta.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+
+				$sectionTitle="Bou";
+				$sectionDescription="WIP";
+				$sectionUrl="/images/infantil/bou.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+
+				$sectionTitle="Ceptrot";
+				$sectionDescription="WIP";
+				$sectionUrl="/images/infantil/ceptrot.jpg";
+				require(getPath('/src/pages/components/infantilRow.php')); 
+			?>
 		</div>
 	</div>
 </div>
