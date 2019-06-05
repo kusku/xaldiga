8a4207179da78efaefb7362407f4542c011e1ee7
Marc Cuscullola <mcuscullola@gmail.com>
2019-06-05 23:47:50 +0200

    Dimonis page

--- src/Controller/CorrefocController.php
+++ src/Controller/CorrefocController.php
@@ -8,6 +8,94 @@ class CorrefocController extends AbstractController
 {
     public function correfocAction()
     {
+        $genericInfo = array(
+            "sectionFolder" => "correfoc",
+            "title" => "tid_correaigua_title",
+            "description" => "tid_correaigua_desc",
+            "mainImageDescription" => "tid_correaigua_main_image_desc"
+        );
+        return $this->render('web/correfoc.html.twig', ['genericInfo' => $genericInfo]);
+    }
+
+    public function mostraAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function historiaAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function recorregutAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function dimonisAction()
+    {
+        $genericInfo = array(
+            "sectionFolder" => "dimonis",
+            "title" => "tid_correfoc_dimonis_title",
+            "description" => "tid_correfoc_dimonis_desc",
+            "mainImageDescription" => "tid_correfoc_dimonis_main_image_desc"
+        );
+
+        $capgirells = array(
+            "title" => "tid_correfoc_dimonis_capgirells_title",
+            "imageName" => "capgirells.jpg",
+            "description" => "tid_correfoc_dimonis_capgirells_desc"
+        );
+
+        $fogueres = array(
+            "title" => "tid_correfoc_dimonis_fogueres_title",
+            "imageName" => "fogueres.jpg",
+            "description" => "tid_correfoc_dimonis_fogueres_desc"
+        );
+
+        $moixogants = array(
+            "title" => "tid_correfoc_dimonis_moixogants_title",
+            "imageName" => "moixogants.jpg",
+            "description" => "tid_correfoc_dimonis_moixogants_desc"
+        );
+
+        $sectionElements = array($capgirells, $fogueres, $moixogants);
+
+        return $this->render('web/correfoc-page-with-sections.html.twig', ['genericInfo' => $genericInfo, 'sectionElements' => $sectionElements]);
+    }
+
+    public function bestiariAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function figuresAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function musicaAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function tabalersAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function quantsbandAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function sacAction()
+    {
+        return $this->render('web/correfoc.html.twig');
+    }
+
+    public function seguretatAction()
+    {
         return $this->render('web/correfoc.html.twig');
     }
 }
