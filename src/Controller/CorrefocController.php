5306b3d5addcb4b5589e5b4279618f2ecc1e609c
Marc Cuscullola <mcuscullola@gmail.com>
2019-06-08 11:35:30 +0200

    Added bestiari page

--- src/Controller/CorrefocController.php
+++ src/Controller/CorrefocController.php
@@ -66,7 +66,64 @@ class CorrefocController extends AbstractController
 
     public function bestiariAction()
     {
-        return $this->render('web/correfoc.html.twig');
+        $genericInfo = array(
+            "sectionFolder" => "bestiari",
+            "title" => "tid_correfoc_bestiari_title",
+            "description" => "tid_correfoc_bestiari_desc",
+            "mainImageDescription" => "tid_correfoc_bestiari_main_image_desc"
+        );
+
+        $vibria = array(
+            "title" => "tid_correfoc_bestiari_vibria_title",
+            "imageName" => "vibria.jpg",
+            "description" => "tid_correfoc_bestiari_vibria_desc"
+        );
+
+        $drac = array(
+            "title" => "tid_correfoc_bestiari_drac_title",
+            "imageName" => "drac.jpg",
+            "description" => "tid_correfoc_bestiari_drac_desc"
+        );
+
+        $asmodeu = array(
+            "title" => "tid_correfoc_bestiari_asmodeu_title",
+            "imageName" => "asmodeu.jpg",
+            "description" => "tid_correfoc_bestiari_asmodeu_desc"
+        );
+
+        $gargola = array(
+            "title" => "tid_correfoc_bestiari_gargola_title",
+            "imageName" => "gargola.jpg",
+            "description" => "tid_correfoc_bestiari_gargola_desc"
+        );
+
+        $mulassa = array(
+            "title" => "tid_correfoc_bestiari_mulassa_title",
+            "imageName" => "mulassa.jpg",
+            "description" => "tid_correfoc_bestiari_mulassa_desc"
+        );
+
+        $nassutja = array(
+            "title" => "tid_correfoc_bestiari_nassutja_title",
+            "imageName" => "nassutja.jpg",
+            "description" => "tid_correfoc_bestiari_nassutja_desc"
+        );
+
+        $collllarg = array(
+            "title" => "tid_correfoc_bestiari_collllarg_title",
+            "imageName" => "collllarg.jpg",
+            "description" => "tid_correfoc_bestiari_collllarg_desc"
+        );
+
+        $bou = array(
+            "title" => "tid_correfoc_bestiari_bou_title",
+            "imageName" => "bou.jpg",
+            "description" => "tid_correfoc_bestiari_bou_desc"
+        );
+
+        $sectionElements = array($asmodeu, $vibria, $drac, $gargola, $mulassa, $nassutja, $collllarg, $bou);
+
+        return $this->render('web/correfoc-page-with-sections.html.twig', ['genericInfo' => $genericInfo, 'sectionElements' => $sectionElements]);
     }
 
     public function figuresAction()
