d28ef761e2fb8d4b59962035c9143ad8d84c096b
Marc Cuscullola <mcuscullola@gmail.com>
2019-06-22 18:10:55 +0200

    Sending information from controller for calendar

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -8,6 +8,38 @@ class WebController extends AbstractController
 {
     public function homepageAction()
     {
-        return $this->render('web/homepage.html.twig');
+        $calendar1 = array(
+            "day" => "25",
+            "month" => "tid_august",
+            "year" => "2019",
+            "title" => "Moscada Infantil",
+            "hour" => "20:00h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa"
+        );
+
+        $calendar2 = array(
+            "day" => "31",
+            "month" => "tid_august",
+            "year" => "2019",
+            "title" => "La Mostra",
+            "hour" => "21:30h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa"
+        );
+
+        $calendar3 = array(
+            "day" => "2",
+            "month" => "tid_september",
+            "year" => "2019",
+            "title" => "El Correfoc de Manresa",
+            "hour" => "21:30h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa"
+        );
+
+        $calendarElements = array($calendar1, $calendar2, $calendar3);
+
+        return $this->render('web/homepage.html.twig', ['calendarElements' => $calendarElements]);
     }
 }
