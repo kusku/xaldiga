845a64dd29e5ac28e67796329693ef87b94744c2
Marc Cuscullola <mcuscullola@gmail.com>
2019-07-23 22:31:20 +0200

    Implemented calendar page

--- src/Controller/EntitatController.php
+++ src/Controller/EntitatController.php
@@ -15,4 +15,47 @@ class EntitatController extends AbstractController
     {
         return $this->render('web/entitat.html.twig');
     }
+
+    public function agendaAction()
+    {
+        $acte1 = array(
+            "day" => "25",
+            "month" => "tid_august",
+            "year" => "2019",
+            "title" => "Moscada Infantil",
+            "hour" => "20:00h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa",
+            "description" => "Recorregut: Plaça Major, Carrer Sant Miquel, Carrer Sabateria, Carrer Pedregar, Plaça del Carme, Carrer Cap del Rec, Plaça Major."
+        );
+        
+        $acte2 = array(
+            "day" => "31",
+            "month" => "tid_august",
+            "year" => "2019",
+            "title" => "La Mostra",
+            "hour" => "21:30h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa",
+            "description" => "Description"
+        );
+
+        $acte3 = array(
+            "day" => "02",
+            "month" => "tid_september",
+            "year" => "2019",
+            "title" => "El Correfoc",
+            "hour" => "21:30h",
+            "dir" => "Plaça Major",
+            "city" => "Manresa",
+            "description" => "Description"
+        );
+
+        $actes2019 = array($acte1, $acte2, $acte3);
+        $actes2020 = array($acte1, $acte2, $acte3);
+
+        $agendaInfo = [2019 => $actes2019, 2020 => $actes2020];
+
+        return $this->render('web/agenda.html.twig', ['agendaInfo' => $agendaInfo]);
+    }
 }
