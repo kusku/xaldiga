120b2ff8c28877fc7bda57b0704862e6a79d9935
Marc Cuscullola <mcuscullola@gmail.com>
2020-02-24 23:02:07 +0100

    removed commented code

--- assets/js/intranet/components/edit-button.js
+++ assets/js/intranet/components/edit-button.js
@@ -9,22 +9,16 @@ export class EditButton extends React.Component {
     render() {
         let button;
 
-        console.log(1);
         if(this.props.modalDataTarget) {
-            console.log(2);
             button = <button type="button" className="btn btn-info" data-toggle="modal" data-target={this.props.modalDataTarget}><i className="fa fa-pencil"></i></button>
         }
         else if(this.props.handle) {
-            console.log(3);
             button = <button type="button" className="btn btn-info" onClick={(e) => this.props.handle(e, this.props.id) }><i className="fa fa-pencil"></i></button>
         }
-        console.log(4);
 
         return (
             <div className="d-inline p-1">
                 { button }
-                {/* <button type="button" className="btn btn-info" data-toggle="modal" data-target="#exampleModal"><i className="fa fa-pencil"></i></button> */}
-                {/* <button type="button" className="btn btn-info" onClick={(e) => this.props.handle(e, this.props.id) }><i className="fa fa-pencil"></i></button> */}
             </div>
         );
     }
